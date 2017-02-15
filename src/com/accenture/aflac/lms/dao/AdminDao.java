c611dd1ed53f0a89040ee807ff6d1f5b52ed2291
xjj <xjj@192.168.1.125>
2017-02-16 00:26:24 +0800

    因为管理员只有一个，所以没有必要把后台的这两个模块也分开处理。统一放到用户模块。

--- src/com/accenture/aflac/lms/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.aflac.lms.dao;
-
-import com.accenture.aflac.lms.dao.entity.Admin;
-
-public interface AdminDao<T> extends BaseDao<T>{
-
-	Admin login(Admin admin);
-
-}
--- src/com/accenture/aflac/lms/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.aflac.lms.service;
-
-import com.accenture.aflac.lms.dao.entity.Admin;
-
-public interface AdminService {
-
-	Admin login(Admin admin);
-
-}
