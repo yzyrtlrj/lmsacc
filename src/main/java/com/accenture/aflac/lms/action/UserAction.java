55b7a1041eae1d9d35dd14b49340de3e392b4070
xjj <xjj@192.168.1.125>
2017-02-26 21:18:26 +0800

    <value>classpath:/hbm/*.hbm.xml</value> ---> <value>classpath:com/accenture/aflac/lms/hbm</value>

--- src/main/java/com/accenture/aflac/lms/action/UserAction.java
+++ src/main/java/com/accenture/aflac/lms/action/UserAction.java
@@ -10,7 +10,7 @@ import com.accenture.aflac.lms.service.UserService;
 import com.accenture.aflac.lms.util.MD5Util;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
-@Controller
+@Controller("userAction")
 @Scope("prototype")
 public class UserAction extends ActionSupport implements ModelDriven<User>{
 	//模型驱动使用的对象
